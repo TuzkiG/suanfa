9380e8cb71d33fd66841e839f6a3c4c7ab990cbb
TuzkiG <291319399@qq.com>
2021-12-30 19:44:52 +0800

    c#

---
+++ List/IListDS.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suanfa
+{
+    interface IListDS<T>
+    {
+        /// <summary>
+        /// 获取长度
+        /// </summary>
+        /// <returns></returns>
+        int getLength();
+        /// <summary>
+        /// 清除所有元素
+        /// </summary>
+        void Clear();
+        /// <summary>
+        /// 查看是否是空表
+        /// </summary>
+        bool IsEmpty();
+       /// <summary>
+       /// 在最后添加
+       /// </summary>
+       /// <param name="item">数据</param>
+        void Append(T item);
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="item">数据</param>
+        void Remove(T item);
+        /// <summary>
+        /// 插入
+        /// </summary>
+        /// <param name="item">数据</param>
+        /// <param name="pos">位置</param>
+        void Insert(T item,int pos);
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="i">位置</param>
+        /// <returns>删除的数据</returns>
+        T Delect(int i);
+        /// <summary>
+        /// 获取位置的数据
+        /// </summary>
+        /// <param name="i">位置</param>
+        /// <returns>对应位置的数据</returns>
+        T GetElem(int i);
+        /// <summary>
+        /// 查看当前数据在什么位置
+        /// </summary>
+        /// <param name="value">数据</param>
+        /// <returns>位置</returns>
+        int locate(T value);
+
+        
+
+    }
+}
---
+++ List/SeqList.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suanfa.List
+{
+    class SeqList<T> : IListDS<T>
+    {
+        private int maxsize;  //数组最大容量
+        private T[] data;   //数组
+        public int mLast { private set; get; }   //数组最后一个元素的位置
+
+        public int MaxSize
+        {
+            get { return maxsize; }
+            set { maxsize = value; }
+        }
+        public T this[int index]
+        {
+            get
+            {
+                return GetElem(index);
+            }
+
+            set
+            {
+                Insert(value, index);
+            }
+        }
+
+
+        public SeqList(int size)
+        {
+            data = new T[size];
+            maxsize = size;
+            mLast = -1;
+        }
+
+        public void Append(T item)
+        {
+            if (isFull())
+                throw new Exception("List is full");
+            else
+                data[++mLast] = item;
+        }
+
+        public void Clear()
+        {
+            mLast = -1;
+        }
+
+        public T Delect(int i)
+        {
+            T t = default(T);
+            return t;
+        }
+
+        public T GetElem(int i)
+        {
+            if (i > data.Length)
+                throw new Exception("索引超过最大数组");
+            else
+                return data[i];
+        }
+
+        public int getLength()
+        {
+            return mLast;   //因为是返回List长度  而不是创建的数组长度
+        }
+
+        public void Insert(T item, int pos)
+        {
+            if (indexIsValid(pos) && !isFull())
+            {
+                if (pos == mLast + 2)
+                    data[mLast + 1] = item;
+                else
+                {
+                    for (int i = mLast; i >= pos - 1; i--)   //插入的话 需要吧当前索引之后的数据往后挪动
+                    {
+                        data[i + 1] = data[i];
+                    }
+
+                    data[pos - 1] = item;
+                }
+
+                ++mLast;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return mLast == -1;
+        }
+
+        public int locate(T value)
+        {
+            int mlocate = 0;
+
+            return mlocate;
+        }
+
+        public void Remove(T item)
+        {
+
+        }
+        public bool isFull()
+        {
+            return mLast == maxsize - 1;
+        }
+
+        private bool indexIsValid(int index)
+        {
+            return !(index < 0 || index > mLast + 2);
+        }
+    }
+}
